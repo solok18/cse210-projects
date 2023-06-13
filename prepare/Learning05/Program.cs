d229f1db06d60b13674652568f88ac76f6fa00c7
solok18 <ksolorzano862@gmail.com>
2023-06-12 19:51:48 -0700

    Learnig5 shapes code finish

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square shape1 = new Square("Blue",5);
+        shapes.Add(shape1);
+
+        Rectangle shape2 = new Rectangle("Red", 4, 2);
+        shapes.Add(shape2);
+
+        Circle shape3 = new Circle("Yellow", 2);
+        shapes.Add(shape3);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The shape color is: {color} and its area is {area}");
+        }
     }
 }
